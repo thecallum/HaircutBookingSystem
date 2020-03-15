99ba686aee3f8ddfd0f79a27cd6113d333816b6f
Callum Macpherson <callummac@protonmail.com>
2020-03-15 12:01:27 +0000

    Added bootstrap theme, and updated header component

--- HaircutBookingSystem/App_Start/BundleConfig.cs
+++ HaircutBookingSystem/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace HaircutBookingSystem
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-solar.css",
                       "~/Content/site.css"));
         }
     }
