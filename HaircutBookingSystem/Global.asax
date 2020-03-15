953b217f06251df7168fbfe6384ea8b6ad280c51
Callum Macpherson <callummac@protonmail.com>
2020-03-15 10:44:47 +0000

    Add project files.

---
+++ HaircutBookingSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HaircutBookingSystem.MvcApplication" Language="C#" %>
---
+++ HaircutBookingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HaircutBookingSystem.Startup))]
+namespace HaircutBookingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
